b8c0cd9ebb52f190cc7bc10d8892ab4fe6874bd7
ggheon <60869685+ggheon@users.noreply.github.com>
2020-03-20 12:08:05 +0900

    test
    test

---
+++ data02.java
@@ -0,0 +1,21 @@
+package 변수;
+
+public class data02 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub // ctrl+spacebar
+		// sysout, syso + ctrl space bar
+		// main + space bar
+		// 자바 프로그램으로 나는 어떤 난이도의 코딩을 하고 싶은가?
+		// 자바 프로그램 중 어떤 프로그램을 해보고 싶은가?
+		// 5줄 이상.
+			System.out.println("과목은 자바");
+			System.out.println("초콜렛이 좋아요.");
+			System.out.println("오늘은 운동을 가야겠지..");
+			System.out.println("자바론 어플개발!");
+			System.out.println("얍얍");
+			
+		
+	}
+
+}
